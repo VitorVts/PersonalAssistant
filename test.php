5fcba621692cf30623dec8cd7bcd6a1de23fca11
Vitor Gomes <114713499+VitorVts@users.noreply.github.com>
2025-04-28 08:50:53 -0300

    test commit

--- test.php
+++ test.php
@@ -3,4 +3,5 @@
 https://api.telegram.org/bot7881481989:AAGVeuF5YQKnzCifNd5hHiI2YOPDzZe0OvE/deleteWebhook deletewebohook
 
 
-https://api.telegram.org/bot7881481989:AAGVeuF5YQKnzCifNd5hHiI2YOPDzZe0OvE/setWebhook?url=https://nasty-geckos-lie.loca.lt/PersonalAssistant/webhook.php
+https://api.telegram.org/bot7881481989:AAGVeuF5YQKnzCifNd5hHiI2YOPDzZe0OvE/setWebhook?url=https://small-streets-walk.loca.lt/PersonalAssistant/webhook.php
+    https://small-streets-walk.loca.lt
